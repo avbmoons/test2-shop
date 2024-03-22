d04899446d76b11fc02ed25f1a53483203d9190d
Elena-admin <avb.moons@gmail.com>
2024-03-22 15:24:58 +0300

    remove

--- src/views/About.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="page">
-    <h1>About page</h1>
-  </div>
-</template>
--- vue.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/test2-shop/' : '/',
-  devServer: {
-    proxy: 'http://localhost:3000',
-  },
-};
